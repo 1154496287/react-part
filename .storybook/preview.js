92e7130b28b958344b902a3699e0ef4cdf4099e2
qingdong <lijinglong@192.168.0.105>
2022-05-02 10:19:03 +0800

    配置文件

---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+import "../src/styles/index.scss";
+export const parameters = {
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		expanded: true,
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
